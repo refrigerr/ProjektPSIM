f29ee0e655724a9ddd42b7d24f7b780cbf671af5
mic609 <michu-2001@o2.pl>
2023-06-16 16:55:58 +0200

    Main site added and customized

--- projekt/config/routes.rb
+++ projekt/config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   resources :rooms
   resources :cards
   devise_for :users
+  devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+  resources :users do
+      resources :cards
+  end
+  root 'pages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
